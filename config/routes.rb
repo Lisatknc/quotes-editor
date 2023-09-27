6af42407de1456590acb205bf3c230b7e2e19a8d
Lisa Takenouchi <rhrbanb@gmail.com>
2023-09-27 15:46:41 +0900

    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "pages#home"
+
     resources :quotes do
-      resources :line_item_dates, except: [:index, :show]
+      resources :line_item_dates, except: [:index, :show] do
+        resources :line_items, except: [:index, :show]
+      end
     end
 end
